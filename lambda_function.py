793ebe1a9aac5183bb8172b6515e7f631f12ebd1
Srinivasulu Bommineni (Trianz) <srinivasulu.b@trianz.com>
2023-08-18 20:17:10 +0530

    added demo lambda code

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
 import pandas as pd
 def lambda_handler(event,context);
 d= {'col1':[1,2]'col2': [3,4]}
-dt= pd.DataFrame[data=d]
-print(dt)
-print('done x1')
+df= pd.DataFrame[data=d]
+print(df)
+print('done x1.1')
